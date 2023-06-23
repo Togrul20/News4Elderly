abd004f90f2b2823ae5390ba38b5d0ebd7260de6
Togrul20 <togrul312000@mail.ru>
2023-06-23 18:36:37 +0400

    Bug fixes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,25 +5,6 @@ import "../styles/GoogleTranslate.module.css";
 import Layout from "./Layout";
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      window.addEventListener("load", () => {
-        navigator.serviceWorker
-          .register("/service-worker.js")
-          .then((registration) => {
-            console.log("Service worker registered: ", registration);
-          })
-          .catch((registrationError) => {
-            console.log(
-              "Service worker registration failed: ",
-              registrationError
-            );
-          });
-      });
-    }
-  }, []);
   return (
     <Layout>
       <Component {...pageProps} />
